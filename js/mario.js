cd360b6a2474df6859f9b9b22dfe8454c975c7cb
d_plentz <dudaplentz123@gmail.com>
2021-03-18 11:56:41 -0300

    Ativ 3

---
+++ js/mario.js
@@ -0,0 +1,57 @@
+class mario
+{
+    
+    constructor()
+    {
+        this.personagem;
+        this.vivo = true;
+        this.tamanho = "pequeno";
+        this.poder = "nenhum";
+    }
+
+    retornarInfo(){
+        let retorno;
+        
+            retorno = "<br/>----- Info ----- <br/>"
+            retorno += " Personagem: " + this.personagem;
+            retorno += "<br/> Vivo? " + this.vivo;
+            retorno += "<br/> Tamanho: " + this.tamanho;
+            retorno += "<br/> Poder: " + this.poder;
+            
+            document.write(retorno);
+    }
+
+    pegarItem(item){
+        switch (item) {
+            case "cogumelo":
+                this.tamanho = "grande";
+                break;
+            case "pena":
+                this.tamanho = "grande";
+                this.poder = "voar";
+                break;
+            case "flor":
+                this.tamanho = "grande";
+                this.poder = "queimar";
+                break;
+            default:
+                return false;
+                break;
+        }
+    }
+
+    serAtacado(){
+        let vivo;
+        
+            if(this.tamanho == "grande" || this.poder != "nenhum"){
+                vivo = "vivo";
+                this.poder = "nenhum";
+                this.tamanho = "pequeno";
+            
+            } else{
+                this.vivo = false;
+                vivo = "morto";
+            }
+            return vivo;
+    }
+}
---
+++ js/objetosmario.js
@@ -0,0 +1,5 @@
+var primeiraTentativa = new mario();
+    primeiraTentativa.personagem = "Mário";
+
+var segundaTentativa = new mario();
+    segundaTentativa.personagem = "Luígi";
