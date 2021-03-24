e771d2a35517927c0aaf14198a4b04844fda4ffe
d_plentz <dudaplentz123@gmail.com>
2021-03-24 12:12:12 -0300

    Mudanças celular

--- js/objetoscelular.js
+++ js/objetoscelular.js
@@ -1,12 +1,33 @@
+var objCelular = new celular();
 function criarObjeto()
 {
-    var objCelular = new celular();
     objCelular.modelo = txtModelo.value;
     objCelular.marca = txtMarca.value;
     alert("celular criado com sucesso");
 }
 
 function exibir()
+{     
+    if (objCelular != null) {
+        objCelular.exibirNaDiv(divUm);
+        alert("Deu certo");
+    } else{
+        alert("Deu errado");
+    }   
+}
+
+function carregarBat()
 {
+    let valor;
+    valor = carga.value;
+    objCelular.carregarBateria(valor);
+    objCelular.exibirNaDiv(divUm);
+}
+
+function usar()
+{
+    let nome;
+    nome = app.value;
+    objCelular.usarApp(nome); 
     objCelular.exibirNaDiv(divUm);
 }
